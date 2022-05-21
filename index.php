b99b9d5a44d73d701f46f2eeb6ce07419e2d98d6
xcoders <57432765+xcoders-hub@users.noreply.github.com>
2022-05-21 17:04:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,13 @@ if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
 include 'madeline.php';
-//include 'vendor/autoload.php';
-use Amp\Http\Server\RequestHandler\CallableRequestHandler;
-use Amp\Http\Server\HttpServer;
-use Amp\Http\Server\Request;
-use Amp\Http\Server\Response;
-use Amp\Http\Status;
-use Amp\Socket\Server;
-use Psr\Log\NullLogger;
 
 $MadelineProto = new \danog\MadelineProto\API('session.madeline');
-$Mmsgid= 'BQACAgUAAxUHYoaeqpXDTkhwShi-zDb7eIPHr5AAAmAGAAIqoTBUHCxEZq_Kev4gBA';
 $MadelineProto->start();
+
 $me = $MadelineProto->getSelf();
+
 $MadelineProto->logger($me);
+echo 'Started...';
+exit();
 ?>
